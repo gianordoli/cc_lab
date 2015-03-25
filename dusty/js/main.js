99e4c5768bfff2fe6e6a8a8e0ff92d4231ba7336
gianordoli <gianordoligabriel@yahoo.com.br>
2015-03-24 20:39:53 -0400

    Test

--- dusty/js/main.js
+++ dusty/js/main.js
@@ -2,7 +2,8 @@
 
 	//Particles
 	var dust;
-	var mouseRadius;	//Size of the mouze
+	var nParticles = 8000;
+	var mouseRadius;		//Size of the mouze
 	var isPressed;
 	var mouseX = 0;
 	var mouseY = 0;
@@ -11,24 +12,26 @@
 	/*---------- FUNCTIONS ----------*/							
 
 	function setup(){
-		dust = new Array(40000);
+		dust = {};
 		mouseRadius = 10;
 		isPressed = false;
 		mouseX = 0;
 		mouseY = 0;	
 
-		for(var i = 0; i < dust.length; i ++){
+		for(var i = 0; i < nParticles; i ++){
 			var particle = $('<div class="particle"></div>');
-			var x = Math.round(Math.random()*window.innerWidth/4)*4;
-			var y = Math.round(Math.random()*window.innerHeight/4)*4;
+			var x = Math.round(Math.random()*window.innerWidth/2)*2;
+			var y = Math.round(Math.random()*window.innerHeight/2)*2;
 			$(particle).css({
 				'left': x,
 				'top': y,
 				'background-color': 'rgb(200, 165, 140)',
 				'opacity': (Math.random() + 0.3).toFixed(1)
 			})
-			$(particle).attr('id', x + ':' + y);
+			// $(particle).attr('id', x + ':' + y);
 			$('body').append(particle);
+			dust[x] = {};
+			dust[x][y] = particle;
 		}
 
 		/*---------- LISTENERS ----------*/
@@ -54,41 +57,43 @@
 	// var n = 0;
 	function draw(){		
 
-		for(var x = mouseX - mouseRadius/2; x < mouseX + mouseRadius/2; x++){
-			for(var y = mouseY - mouseRadius/2; y < mouseY + mouseRadius/2; y++){
-				// $('#'+x+':'+y).css('background-color', 'red');
-				$('.particle').css('background-color', 'red');
-				console.log($('#'+x+':'+y).length);
+		for(var x = mouseX - mouseRadius; x < mouseX + mouseRadius; x += 2){
+			for(var y = mouseY - mouseRadius; y < mouseY + mouseRadius; y += 2){
+				if(x in dust){
+					if(y in dust[x]){
+
+						var obj = dust[x][y];
+						console.log(obj);
+
+						$(obj).css('background-color', 'red');		
+
+						//Calculating the angle							
+						var offset = $(obj).offset();
+						var angle = calculateAngle(offset.left, offset.top);
+
+						//Calculating the distance							
+						var dist;
+
+						if( (mouseY - offset.top) == 0 ){
+							dist = (mouseX - offset.left) / Math.cos( angle );
+						}else{
+							dist = (mouseY - offset.top) / Math.sin( angle );
+						}
+					
+						//Detecting collision
+						if(dist < mouseRadius){
+							//console.log('yay');
+							$(obj).css({
+				                'left': mouseX - Math.cos(angle) * mouseRadius,
+				                'top': mouseY - Math.sin(angle) * mouseRadius
+							});
+
+						}
+
+					}
+				}
 			}
 		}
-
-		// //Draws the squares
-		// for(var i = 0; i < dust.length; i ++){
-		// 	// console.log(i);
-				
-		// 	//Calculating the angle							
-		// 	var offset = $('#'+i).offset();
-		// 	var angle = calculateAngle(offset.left, offset.top);
-
-		// 	//Calculating the distance							
-		// 	var dist;
-
-		// 	if( (mouseY - offset.top) == 0 ){
-		// 		dist = (mouseX - offset.left) / Math.cos( angle );
-		// 	}else{
-		// 		dist = (mouseY - offset.top) / Math.sin( angle );
-		// 	}
-		
-		// 	//Detecting collision
-		// 	if(dist < mouseRadius){
-		// 		//console.log('yay');
-		// 		$('#'+i).css({
-	 //                'left': mouseX - Math.cos(angle) * mouseRadius,
-	 //                'top': mouseY - Math.sin(angle) * mouseRadius
-		// 		});
-
-		// 	}
-		// }
 	}
 
 	/*---------- AUXILIAR FUNCTIONS ----------*/
