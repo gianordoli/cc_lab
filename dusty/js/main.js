a8ce5ab9fce64d67a32cea8d2f4740060c9f264f
gianordoli <gianordoligabriel@yahoo.com.br>
2015-03-24 20:17:52 -0400

    Revert 3d09a0b..5ab6b07
    This rolls back to commit 3d09a0b49ed3119dbd869b666ab8146a621225bf.

--- dusty/js/main.js
+++ dusty/js/main.js
@@ -1,19 +1,7 @@
-var canvas = document.getElementById('myCanvas');
-
-//Check if the browser supports canvas
-if(canvas.getContext){
 	/*---------- VARIABLES ----------*/
-	//Getting canvas context
-	var ctx = canvas.getContext('2d');
-
-	//Canvas position
-	var canvasPosition;
-
-	//Canvas size adjustment for Chrome and Firefox
-	var ratio;
 
 	//Particles
-	var dust;			//Array of particles
+	var dust;
 	var mouseRadius;	//Size of the mouze
 	var isPressed;
 	var mouseX = 0;
@@ -22,110 +10,85 @@ if(canvas.getContext){
 
 	/*---------- FUNCTIONS ----------*/							
 
-	//Resizing the canvas to the full window size
-	function canvasResize(){
-		screenWidth = window.innerWidth;
-		screenHeight = window.innerHeight;
-
-		canvasPosition = canvas.getBoundingClientRect(); // Gets the canvas position
-		canvas.width = screenWidth;
-		canvas.height = screenHeight;
-	}
-
-
-	//Adjusting the size to work with Firefox and Chrome at retina resolutions 
-	function canvasAdjustment(){
-    // finally query the various pixel ratios
-    var devicePixelRatio  = window.devicePixelRatio || 1;
-    //console.log(devicePixelRatio);
-    var backingStoreRatio = ctx.webkitBackingStorePixelRatio ||
-                            ctx.mozBackingStorePixelRatio ||
-                            ctx.msBackingStorePixelRatio ||
-                            ctx.oBackingStorePixelRatio ||
-                            ctx.backingStorePixelRatio || 1;
-
-    ratio = devicePixelRatio / backingStoreRatio;
-    //console.log(ratio);
-    
-    // upscale the canvas if the two ratios don't match
-	    if (devicePixelRatio !== backingStoreRatio) {
-
-	        var oldWidth = canvas.width;
-	        var oldHeight = canvas.height;
-
-	        canvas.width = oldWidth * ratio;
-	        canvas.height = oldHeight * ratio;
-
-	        canvas.style.width = oldWidth + 'px';
-	        canvas.style.height = oldHeight + 'px';
-
-			ctx.scale(ratio, ratio);				        
-	    }
-  	}
-
 	function setup(){
 		dust = new Array(40000);
-		mouseRadius = 20;
+		mouseRadius = 10;
 		isPressed = false;
 		mouseX = 0;
 		mouseY = 0;	
 
 		for(var i = 0; i < dust.length; i ++){
-			var particle = new Object;
-			particle.posX = Math.round(Math.random()*canvas.width/ratio);
-			particle.posY = Math.round(Math.random()*canvas.height/ratio);
-			//.toFixed adjust to a specific number of decimals
-			particle.opacity = (Math.random() + 0.3).toFixed(1);
-			dust[i] = particle;
-
-			//console.log("x: " + dust[i].posX + ", y: " + dust[i].posY + ", alpha: " + dust[i].opacity);
-			//console.log("x: " + dust[i].posX);
-
+			var particle = $('<div class="particle"></div>');
+			var x = Math.round(Math.random()*window.innerWidth/4)*4;
+			var y = Math.round(Math.random()*window.innerHeight/4)*4;
+			$(particle).css({
+				'left': x,
+				'top': y,
+				'background-color': 'rgb(200, 165, 140)',
+				'opacity': (Math.random() + 0.3).toFixed(1)
+			})
+			$(particle).attr('id', x + ':' + y);
+			$('body').append(particle);
 		}
-		
-		//myLoop = setInterval(draw, 60);
-		for(var i = 0; i < dust.length; i ++){
-			ctx.fillStyle = 'rgba(200, 165, 140,'+ dust[i].opacity + ')';
-			ctx.fillRect(dust[i].posX, dust[i].posY, 2, 2);
-		}
-		draw();
+
+		/*---------- LISTENERS ----------*/
+		$(document).mousemove(function(evt){			
+	        // console.log(mouseX);
+			if(isPressed){
+				mouseX = event.pageX;
+		        mouseY = event.pageY;				
+				draw();
+			}
+		});
+
+		$(document).bind('mousedown', function(evt){
+			isPressed = true;	// Set my "isPressed" variable to true
+			draw();
+		});
+
+		$(document).bind('mouseup', function(evt){
+			isPressed = false;	// Set my "isPressed" variable to false
+		});		
 	}						
 
 	// var n = 0;
-	function draw(){
+	function draw(){		
 
-		//console.log('yes');
-		// console.log('called draw function' + n);
-		//Clears the background
-		ctx.fillStyle = 'white';
-		ctx.fillRect(0, 0, canvas.width/ratio, canvas.height/ratio);							
-
-		//Draws the squares
-		for(var i = 0; i < dust.length; i ++){
-
-			//Calculating the angle							
-			var angle = calculateAngle(dust[i].posX, dust[i].posY);
-
-			//Calculating the distance							
-			var dist;
-
-			if( (mouseY - dust[i].posY) == 0 ){
-				dist = (mouseX - dust[i].posX) / Math.cos( angle );
-			}else{
-				dist = (mouseY - dust[i].posY) / Math.sin( angle );
+		for(var x = mouseX - mouseRadius/2; x < mouseX + mouseRadius/2; x++){
+			for(var y = mouseY - mouseRadius/2; y < mouseY + mouseRadius/2; y++){
+				// $('#'+x+':'+y).css('background-color', 'red');
+				$('.particle').css('background-color', 'red');
+				console.log($('#'+x+':'+y).length);
 			}
-		
-			//Detecting collision
-			if(dist < mouseRadius){
-				//console.log('yay');
-                dust[i].posX = mouseX - Math.cos(angle) * mouseRadius;
-                dust[i].posY = mouseY - Math.sin(angle) * mouseRadius;
-			}
-
-			ctx.fillStyle = 'rgba(200, 165, 140,'+ dust[i].opacity + ')';
-			ctx.fillRect(dust[i].posX, dust[i].posY, 2, 2);				
 		}
 
+		// //Draws the squares
+		// for(var i = 0; i < dust.length; i ++){
+		// 	// console.log(i);
+				
+		// 	//Calculating the angle							
+		// 	var offset = $('#'+i).offset();
+		// 	var angle = calculateAngle(offset.left, offset.top);
+
+		// 	//Calculating the distance							
+		// 	var dist;
+
+		// 	if( (mouseY - offset.top) == 0 ){
+		// 		dist = (mouseX - offset.left) / Math.cos( angle );
+		// 	}else{
+		// 		dist = (mouseY - offset.top) / Math.sin( angle );
+		// 	}
+		
+		// 	//Detecting collision
+		// 	if(dist < mouseRadius){
+		// 		//console.log('yay');
+		// 		$('#'+i).css({
+	 //                'left': mouseX - Math.cos(angle) * mouseRadius,
+	 //                'top': mouseY - Math.sin(angle) * mouseRadius
+		// 		});
+
+		// 	}
+		// }
 	}
 
 	/*---------- AUXILIAR FUNCTIONS ----------*/
@@ -133,51 +96,7 @@ if(canvas.getContext){
 		var angle = Math.atan2(mouseY - y, mouseX - x);
 		//console.log('called calculateAngle function');
 		return angle;
-	}
-
-	function getMousePos(evt){
-		var newMouseX = evt.clientX - canvasPosition.left;
-		var newMouseY = evt.clientY - canvasPosition.top;
-		if(Math.abs(mouseX - newMouseX) > 100 && Math.abs(mouseY - newMouseY) > 100){
-			draw();
-		}		
-		console.log(Math.abs(mouseX - newMouseX));
-		console.log(Math.abs(mouseY - newMouseY)
-		mouseX = newMouseX;
-		mouseY = newMouseY;
-		//You have to use clientX! .x doesn't work with Firefox!
-	}
-
-	/*---------- LISTENERS ----------*/
-	canvas.addEventListener('mousemove', function(evt){
-		if(isPressed){
-			// console.log(e.x);
-			// console.log(e.y);
-			getMousePos(evt);
-			draw();
-		}
-	}, false);
-
-	canvas.addEventListener('mousedown', function(evt){
-		isPressed = true;	// Set my "isPressed" variable to true
-	}, false);
-
-	canvas.addEventListener('mouseup', function(evt){
-		isPressed = false;	// Set my "isPressed" variable to false
-	}, false);						
-
-	//Resizing the canvas
-	canvasResize();
-
-	//Adjusting for retina
-	//canvasAdjustment();
-	ratio = 1;
+	}			
 
 	//Calling the setup function
 	setup();		
-
-
-//If the browser doesn't support canvas
-}else{
-	alert('Your browser doesn\'t support canvas');
-}
